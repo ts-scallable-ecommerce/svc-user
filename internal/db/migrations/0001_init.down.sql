7811d43c8641bad558bf1786a39c558b33491d9b
Rezi Hardiansyah <rezi_hardiansyah@circles.asia>
2025-09-26 06:58:59 +0700

    remove all

--- internal/db/migrations/0001_init.down.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS outbox;
-DROP TABLE IF EXISTS refresh_tokens;
-DROP TABLE IF EXISTS role_permissions;
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS permissions;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS users;
